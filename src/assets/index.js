36964b34c18cad0a746d935ee3840691fb0fd34c
Amarjeet2909 <ak4348225@gmail.com>
2024-01-12 21:24:19 +0530

    Initial commit

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,7 +1,6 @@
 import logo from "./logo.png";
 import backend from "./backend.png";
 import creator from "./creator.png";
-import mobile from "./mobile.png";
 import web from "./web.png";
 import github from "./github.png";
 import menu from "./menu.svg";
@@ -20,13 +19,6 @@ import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
-
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
-
-import carrent from "./carrent.png";
 import jobit from "./jobit.png";
 import tripguide from "./tripguide.png";
 
@@ -34,7 +26,6 @@ export {
   logo,
   backend,
   creator,
-  mobile,
   web,
   github,
   menu,
@@ -52,11 +43,6 @@ export {
   tailwind,
   typescript,
   threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
-  carrent,
   jobit,
   tripguide,
 };
